c32ab88af5f4a95aa2b14330116bec33c69224a9
LudvigFaergemann <90576027+LudvigFaergemann@users.noreply.github.com>
2023-12-07 01:14:51 +0100

    Add form inputs and handle listing functionality

--- screens/YourItemsScreen.js
+++ screens/YourItemsScreen.js
@@ -1,19 +1,79 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, Button, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
 import { styles } from "../styles/styles.js";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { firebase } from "../services/firebase";
+import { auth } from "../services/firebase.js";
+import { getFirestore, collection, query, orderBy, limit, addDoc, serverTimestamp } from "firebase/firestore";
 
 // In this screen, the user will be able to see all the items they have listed for rent.
 // They will also be able to add new items to the list, and edit or delete existing items.
-// In the future, we will add calendar functionality to show when the item is available for rent.
 // When listing an item, the user has to fill out a form with datails about the item.
-// Details include: model, make, description, category, price (per day in DKK), location, and pictures.
+// Details include: model, make, description, category, price (per day in DKK), adress, and pictures.
 
+// In the future, we will add calendar functionality to show when the item is available for rent.
 const YourItemsScreen = () => {
+  const [model, setModel] = useState("");
+  const [make, setMake] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [adress, setAdress] = useState("");
+  const [pictures, setPictures] = useState([]);
+
+  const handleListing = async () => {
+    try {
+      const db = getFirestore();
+
+      await addDoc(collection(db, "items"), {
+        model: model,
+        make: make,
+        description: description,
+        category: category,
+        price: price,
+        adress: adress,
+        pictures: pictures,
+        createdAt: serverTimestamp(),
+        userId: auth.currentUser.uid,
+      });
+
+      // Clear the form
+      setModel("");
+      setMake("");
+      setDescription("");
+      setCategory("");
+      setPrice("");
+      setAdress("");
+      setPictures([]);
+    } catch (error) {
+      console.warn("FirebaseError:", error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>YourItemsScreen</Text>
-    </View>
-  )
-}
+      <Text>Mærke:</Text>
+      <TextInput value={make} onChangeText={setMake} style={styles.input} />
 
-export default YourItemsScreen
+      <Text>Model:</Text>
+      <TextInput value={model} onChangeText={setModel} style={styles.input} />
+
+      <Text>Beskrivelse:</Text>
+      <TextInput value={description} onChangeText={setDescription} style={styles.input} />
+
+      <Text>Kategori:</Text>
+      <TextInput value={category} onChangeText={setCategory} style={styles.input} />
+
+      <Text>Pris (per dag):</Text>
+      <TextInput value={price} onChangeText={setPrice} style={styles.input} />
+
+      <Text>Adresse:</Text>
+      <TextInput value={adress} onChangeText={setAdress} style={styles.input} />
+
+      <Button title="Opret udlejningsgenstand" onPress={handleListing} />
+    </View>
+  );
+};
+
+export default YourItemsScreen;
--- styles/styles.js
+++ styles/styles.js
@@ -1,11 +1,18 @@
-import { StyleSheet } from 'react-native'
-// Note that the styles in this file 
+import { StyleSheet } from "react-native";
+// Note that the styles in this file
 // are not being used in the ProfileScreen component
 // as well as the LoginScreen component.
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+    marginBottom: 10,
+    width: 200,
+  },
+});
