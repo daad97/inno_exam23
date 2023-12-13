fc658b0b865be10fa01f26bc0590e2d43489f0bc
LudvigFaergemann <90576027+LudvigFaergemann@users.noreply.github.com>
2023-12-13 15:15:23 +0100

    Add search functionality and add styling to SearchScreen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,31 +1,87 @@
-import React, { useState } from "react";
-import { Text, View, Button, TextInput } from "react-native";
-import { styles } from "../styles/styles.js";
+import React, { useEffect, useState } from 'react';
+import { getFirestore, collection, query, getDocs } from 'firebase/firestore';
+import { Text, View, FlatList, TextInput, StyleSheet } from 'react-native';
 
-// På denne skærm vil brugeren vælge datoerne for udlejningen,
-// og derefter vil appen kontrollere, om varen er tilgængelig til udlejning på disse datoer.
-// Hvis den er det, vil brugeren kunne fortsætte til næste skærm,
-// hvor de vil kunne sende besked til ejeren af varen for at arrangere udlejningen.
 
 const SearchScreen = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const db = getFirestore();
+  const itemsRef = collection(db, "items");
+  const q = query(itemsRef);
 
-  const searchItems = () => {
-    // Implement your logic to search items here
+  const [items, setItems] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const fetchData = async () => {
+    const querySnapshot = await getDocs(q);
+    const itemsData = querySnapshot.docs.map((doc) => doc.data());
+    setItems(itemsData);
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const filteredItems = items.filter(item => {
+    return item.category.toLowerCase().includes(searchQuery.toLowerCase());
+  });
 
   return (
     <View style={styles.container}>
-      <Text>Hvad skal du bruge?</Text>
       <TextInput
-        style={styles.input}
-        value={searchTerm}
-        onChangeText={setSearchTerm}
-        placeholder="Skriv navn på produkt"
+        style={styles.searchBar}
+        placeholder="Søg efter kategori"
+        onChangeText={text => setSearchQuery(text)}
+        value={searchQuery}
       />
-      <Button title="Søg" onPress={searchItems} />
+      <FlatList
+        data={filteredItems}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <View>
+              <Text style={styles.itemText}>{item.category}</Text>
+              <Text style={styles.itemText}>{item.model}</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  searchBar: {
+    fontSize: 18,
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    marginBottom: 10,
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  itemText: {
+    fontSize: 16,
+  },
+});
 
 export default SearchScreen;
--- styles/styles.js
+++ styles/styles.js
@@ -5,6 +5,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginVertical: 10,
     alignItems: "center",
     justifyContent: "center",
   },
